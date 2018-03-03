dcb6c08572e845594cb79a89997bbf55056cf39a
Mauricio Garduño <mau.gar.gonz@icloud.com>
2018-03-03 13:46:57 -0600

    update
    index

--- index.php
+++ index.php
@@ -11,11 +11,7 @@
 
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-<<<<<<< HEAD
     <title>Hackcity</title>
-=======
-<<<<<<< HEAD
->>>>>>> ea5183d6d1f3981ba6b896206be44ae3d42b7856
     <style>
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
@@ -29,58 +25,7 @@
         padding: 0;
       }
     </style>
-<<<<<<< HEAD
 
-=======
-    <script>
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-      var map;
-      var infowindow;
-
-      function initMap() {
-        var pyrmont = {lat: -33.867, lng: 151.195};
-
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 15
-        });
-
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: pyrmont,
-          radius: 500,
-          type: ['store']
-        }, callback);
-      }
-
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      }
-
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
-    </script>
-=======
->>>>>>> a837200ac223ada041a452c307495b6354a34031
->>>>>>> ea5183d6d1f3981ba6b896206be44ae3d42b7856
   </head>
 
   <body>
